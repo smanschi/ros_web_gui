ad87744344198c6592b7ca12c98fe9b89ea10279
Simon Manschitz <simon.manschitz@gmx.de>
2020-11-03 19:50:57 +0100

    page is now scrollable and menu entries are sorted

--- ros_web_gui/menu.py
+++ ros_web_gui/menu.py
@@ -22,9 +22,9 @@ def get_items(data, active_item=None):
             node_names.add(node)
 
     # Navigation
-    nav_node_items =    [{'href': url_for('node.get_node_info', name=node), 'caption': node} for node in node_names]
-    nav_topic_items =   [{'href': url_for('topic.get_topic_info', name=topic), 'caption': topic} for topic in topic_names]
-    nav_service_items = [{'href': url_for('service.get_service_info', name=service), 'caption': service} for service in service_names]
+    nav_node_items =    [{'href': url_for('node.get_node_info', name=node), 'caption': node} for node in sorted(node_names)]
+    nav_topic_items =   [{'href': url_for('topic.get_topic_info', name=topic), 'caption': topic} for topic in sorted(topic_names)]
+    nav_service_items = [{'href': url_for('service.get_service_info', name=service), 'caption': service} for service in sorted(service_names)]
 
     if active_item is not None:
         nav_node_items = [{**item, 'active': True if item['href'] == active_item else False} for item in nav_node_items]
