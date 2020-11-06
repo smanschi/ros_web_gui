4f566cd3220bea24af4fc1df8b1ea9c602a59f12
Simon Manschitz <simon.manschitz@gmx.de>
2020-11-06 22:14:24 +0100

    nicer rendering of params with syntax highlighting

--- ros_web_gui/param.py
+++ ros_web_gui/param.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, Markup, Response, render_template, url_for
+from flask import Blueprint, Markup, Response, escape, render_template, url_for
 from . import menu, ros
 import base64
 from io import BytesIO, StringIO
@@ -11,52 +11,25 @@ import yaml
 
 bp = Blueprint('param', __name__, url_prefix='/param')
 
-@bp.route('/')
-def get_param_overview():
-    # Get info about nodes
-    data = ros.get_info()
-
-    # Get menu items
-    menu_items = menu.get_items(data)
-
-    # Iterate over nodes
-    content = ''
-    items = menu_items['nav_param_items']
-
-    # Return rendered template
-    return render_template('base_with_list.html', title=f'List of Parameters',
-                            active_menu_item='param',
-                            content=content,
-                            items=items,
-                            **menu_items)
-
 def get_text(content):
     buff = ''
     if type(content) == list:
-        #buff += '<ul style="list-style-type:none;">\n'
         for ele in content:
-            #buff += '<li>'
             buff += get_text(ele)
-            #buff += '</li>\n'
-        #buff += '</ul>\n'
     elif type(content) == dict:
         for key, val in content.items():
-            buff += key + ':\n'
-            buff += '<div style="display:inline-block; vertical-align: top">\n'
+            buff += f'/{key}'
+            if type(val) is not dict:
+                buff += '<br />\n'
             buff += get_text(val)
-            buff += '</div><br />\n'
     else:
-        buff += str(content) + '<br />'
+        if type(content) == str and len(content) > 5 and content[:5] == '<?xml':
+            buff +=  f'<pre style="display: inline-block"><code class="html">{escape(content)}</code></pre><br />\n'
+        else:
+            buff +=  f'<pre style="display: inline-block"><code class="plaintext">{escape(content)}</code></pre><br />\n'
     return buff
 
-@bp.route('/<path:name>')
-def get_param_info(name):
-    if not name.startswith('/'):
-        name = '/' + name
-
-    # Get info about param
-    data = ros.get_info()
-
+def get_param(name):
     # Generate content
     try:
         content = rosparam.get_param(name)
@@ -68,6 +41,54 @@ def get_param_info(name):
 
     # Format param info
     content = Markup('<div class="w3-container">' + content + '</div>')
+    return content
+
+# @bp.route('/')
+# def get_param_overview():
+#     # Get info about nodes
+#     data = ros.get_info()
+
+#     # Get menu items
+#     menu_items = menu.get_items(data)
+
+#     # Iterate over nodes
+#     content = ''
+#     items = menu_items['nav_param_items']
+
+#     # Return rendered template
+#     return render_template('base_with_list.html', title=f'List of Parameters',
+#                             active_menu_item='param',
+#                             content=content,
+#                             items=items,
+#                             **menu_items)
+
+@bp.route('/')
+def get_param_overview():
+    # Get info about nodes
+    data = ros.get_info()
+
+    # Get menu items
+    menu_items = menu.get_items(data)
+
+    # Iterate over nodes
+    content = get_param('/')
+
+    # Return rendered template
+    return render_template('base.html', title=f'List of Parameters',
+                            active_menu_item='param',
+                            content=content,
+                            **menu_items)
+
+@bp.route('/<path:name>')
+def get_param_info(name):
+    if not name.startswith('/'):
+        name = '/' + name
+
+    # Get info about param
+    data = ros.get_info()
+
+    # Generate content
+    content = get_param(name)
 
     # Current url
     url = url_for('param.get_param_info', name=name)
