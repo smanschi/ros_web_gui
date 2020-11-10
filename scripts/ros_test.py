f210486c8d6dc86db16950996f2e3ef0b7afc0b6
Simon Manschitz <simon.manschitz@honda-ri.de>
2020-11-10 15:26:46 +0100

    made topic_name a variable

--- scripts/ros_test.py
+++ scripts/ros_test.py
@@ -5,13 +5,14 @@ import rostopic
 from std_msgs.msg import String
 
 def callback(data):
-    rospy.loginfo(f"I heard {data.data}")
+    rospy.loginfo(f"Got message: {data}")
 
-topic_type = rostopic.get_topic_type('/chatter')
+topic_name = '/chatter'
+topic_type = rostopic.get_topic_type(topic_name)
 data_class = roslib.message.get_message_class(topic_type[0])
 
 rospy.init_node('ros_test')
-rospy.Subscriber("chatter", data_class, callback)
+rospy.Subscriber(topic_name, data_class, callback)
 
 # spin() simply keeps python from exiting until this node is stopped
 rospy.spin()
