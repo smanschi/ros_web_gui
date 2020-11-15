baa544a55e05633f7af1e4c14856ab6193ba31a4
smanschi <simon.manschitz@gmx.de>
2020-11-15 11:56:15 +0100

    removed venv activation from run script

--- scripts/run_flask.sh
+++ scripts/run_flask.sh
@@ -3,5 +3,4 @@ export FLASK_APP=ros_web_gui
 export FLASK_ENV=development
 BASEDIR=$(dirname "$0")
 cd ${BASEDIR}/..
-source venv/bin/activate
 flask run --no-reload
--- scripts/run_ros.sh
+++ scripts/run_ros.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 BASEDIR=$(dirname "$0")
 cd ${BASEDIR}/..
-source venv/bin/activate
 roscore &
 scripts/publisher.py &
 scripts/subscriber.py && fg
