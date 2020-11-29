1e1463aa6395feed5e0f35fce6af1e40568f5506
Simon Manschitz <simon.manschitz@gmx.de>
2020-11-29 21:16:13 +0100

    started working on pip package setup

--- setup.py
+++ setup.py
@@ -1,3 +1,23 @@
-from setuptools import setup
+import setuptools
 
-setup()
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ros_web_gui",
+    version="0.0.1",
+    author="Simon Manschitz",
+    author_email="simon.manschitz@gmx.de",
+    description="A web gui for ROS (robot operating system)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/smanschi/ros_web_gui",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    scripts=['scripts/ros_web_gui'],
+)
